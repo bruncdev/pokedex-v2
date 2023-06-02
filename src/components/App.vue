58adfffe61bfb9c61aeb5535f3c1f95ebe8ddd78
nwplul <snapfx32@gmail.com>
2023-06-02 00:12:59 -0300

    using colorthief to merge a predominant pokemon color to main background

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div :style="{ backgroundColor: pokemonBgColor }" class="flex flex-col items-center justify-center h-screen w-full">
     <div class="py-16 md:py-20 lg:py-24 xl:py-20 w-full">
-      <pokemon-display :pokemon="pokemon" :bg-color="pokemonBgColor"></pokemon-display>
+      <pokemon-display :pokemon="pokemon"></pokemon-display>
     </div>
 
     <div class="mt-8 md:mt-10 lg:mt-12 xl:mt-14">
@@ -13,6 +13,7 @@
 <script>
 import PokemonSearch from './PokemonSearch.vue';
 import PokemonDisplay from './PokemonDisplay.vue';
+import ColorThief from "colorthief";
 
 export default {
   components: {
@@ -32,7 +33,15 @@ export default {
     },
     updatePokemonBgColor() {
       if (this.pokemon) {
-        this.pokemonBgColor = this.pokemon.bgColor;
+        const imageUrl = this.pokemon.sprites?.other['official-artwork'].front_default;
+        const img = new Image();
+        img.src = imageUrl;
+        img.crossOrigin = 'Anonymous';
+        img.addEventListener('load', () => {
+          const colorThief = new ColorThief();
+          const color = colorThief.getColor(img);
+          this.pokemonBgColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+        });
       } else {
         this.pokemonBgColor = '';
       }
